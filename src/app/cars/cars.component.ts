0b05c5b49bba2326d14b42833b0b357c21b7745d
Javier Argueta <jaynextpro@gmail.com>
2020-11-09 18:44:34 -0600

    Data binding

--- src/app/cars/cars.component.ts
+++ src/app/cars/cars.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Car } from '../models/Car'
+import { Car } from '../models/Car';
 
 @Component({
   selector: 'cars',
@@ -7,16 +7,28 @@ import { Car } from '../models/Car'
   styleUrls: ['./cars.component.css']
 })
 export class CarsComponent implements OnInit {
-  public Cars: Car[];  
+  public Cars: Car[];
+  public ActualCar: Array<string>;
+
   constructor() {
   	this.Cars = [
   		new Car("Skyline R34","Nissan",1998),
   		new Car("911","Porshe",1964),
   		new Car("Challenger","Dodge",1970)		
-  	]
+  	];
+
+    this.ActualCar = ["", "", ""];
   }
 
   ngOnInit(): void {
+  }
+
+  public AddCar() {
+    this.Cars.push(new Car((<HTMLInputElement>document.getElementById("Model")).value, (<HTMLInputElement>document.getElementById("Brand")).value, (<HTMLInputElement>document.getElementById("Year")).value));
+  }
+
+  public DeleteCar(index) {
+    this.Cars.splice(index, 1);
   }
 
 }
